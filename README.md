d7235906b43110735dfd9b69ddfe413f1551040a
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2017-05-09 13:34:39 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# skyux-template-library
+
+Template for SKY UX component libraries
+
+- Update `name` in **package.json**
+- Update `output.fileName` and `output.library` in **config/webpack/webpack.prod.config.js**
+- Update `my-module` directory in **src/app/shared**
+
+```
+npm run build
+```
---
+++ config/webpack/webpack.prod.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const webpack = require('webpack');
+const root = require('../utils/helpers').root;
+
+module.exports = {
+  entry: './index.ts',
+  output: {
+    path: path.resolve(__dirname, '..', '..', 'dist', 'bundles'),
+    filename: 'my-module.umd.js',
+    library: 'MyModule',
+    libraryTarget: 'umd'
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: 'ts-loader',
+        options: {
+          compilerOptions: {
+            declaration: false
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    // The (\\|\/) piece accounts for path separators in *nix and Windows
+    new webpack.ContextReplacementPlugin(
+      /angular(\\|\/)core(\\|\/)(esm(\\|\/)src|src)(\\|\/)linker/,
+      root('./src'), // location of your src
+      {} // a map of your routes
+    )
+  ]
+};
