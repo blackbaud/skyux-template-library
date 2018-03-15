57d84409b1569821ebbc98aea2e36cdfabd930ab
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2018-03-15 10:28:35 -0400

    Updated implementation

--- src/app/public/src/modules/sample/sample.module.ts
+++ src/app/public/src/modules/sample/sample.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { SkyAppRuntimeModule } from '@blackbaud/skyux-builder/runtime/runtime.module';
+
 import { LibrarySampleComponent } from './sample.component';
-import { SkyAppRuntimeModule } from '@blackbaud/skyux-builder/runtime';
 
 @NgModule({
   declarations: [
