99d4344be1ec5382b65fe76f66e0c1bc44c7d426
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2018-03-27 08:35:04 -0400

    Added various resources

--- src/app/public/src/modules/sample/sample.component.visual-spec.ts
+++ src/app/public/src/modules/sample/sample.component.visual-spec.ts
@@ -1,23 +1,23 @@
-import {
-  SkyHostBrowser,
-  SkyVisualTest
-} from '@blackbaud/skyux-builder/runtime/testing/e2e';
+// import {
+//   SkyHostBrowser,
+//   SkyVisualTest
+// } from '@blackbaud/skyux-builder/runtime/testing/e2e';
 
-describe('Sample component', () => {
-  beforeEach(() => {
-    SkyHostBrowser.get('/');
-  });
+// describe('Sample component', () => {
+//   beforeEach(() => {
+//     SkyHostBrowser.get('/');
+//   });
 
-  it('should match previous screenshot', () => {
-    SkyVisualTest.compareScreenshot({
-      screenshotName: 'sample'
-    });
-  });
+//   it('should match previous screenshot', () => {
+//     SkyVisualTest.compareScreenshot({
+//       screenshotName: 'sample'
+//     });
+//   });
 
-  it('should match previous screenshot on tiny screens', () => {
-    SkyVisualTest.compareScreenshot({
-      screenshotName: 'sample-xs',
-      breakpoint: 'xs'
-    });
-  });
-});
+//   it('should match previous screenshot on tiny screens', () => {
+//     SkyVisualTest.compareScreenshot({
+//       screenshotName: 'sample-xs',
+//       breakpoint: 'xs'
+//     });
+//   });
+// });
