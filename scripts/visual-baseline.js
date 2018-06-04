af8f5f96d83b31fd41ecee72daecc5927fe7ef34
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2018-06-04 16:24:18 -0400

    Updated visual baseline script

--- scripts/visual-baseline.js
+++ scripts/visual-baseline.js
@@ -19,43 +19,46 @@ function exec(command, opts) {
 
     cp.stdout.on('data', (data) => {
       log(data);
-      output = data.toString('utf8').replace(/\n/g, '');
+      output = data.toString('utf8');
     });
 
+    let error;
     cp.stderr.on('data', (data) => {
-      log(data);
+      error = data.toString('utf8');
     });
 
     cp.on('error', (err) => {
-      log(err);
-      reject(err);
+      error = err;
     });
 
     cp.on('exit', (code) => {
-      resolve({
-        code,
-        output
-      });
+      if (code === 0) {
+        resolve({
+          code,
+          output
+        });
+        return;
+      }
+
+      reject(error);
     });
   });
 }
-
-/**
- * - Login to GitHub
- * - If diffs, commit to remote repo.
- */
 
 let branch;
 let tempDir = '.skypagese2etemp'
 
 exec('git config --get remote.origin.url')
-  .then((result) => exec(`git clone ${result.output} ${tempDir}`))
+  .then((result) => {
+    const gitUrl = result.output.replace(/\n/g, '');
+    return exec(`git clone ${gitUrl} ${tempDir}`);
+  })
 
   // Get name of feature branch:
   // https://stackoverflow.com/a/12142066/6178885
   .then(() => exec('git rev-parse --abbrev-ref HEAD'))
   .then((result) => {
-    branch = result.output;
+    branch = result.output.replace(/\n/g, '');
     return result;
   })
 
@@ -69,13 +72,25 @@ exec('git config --get remote.origin.url')
   .then(() => exec('npm install', { cwd: tempDir }))
   .then(() => exec('skyux e2e', { cwd: tempDir }))
 
-  .then(() => rimraf.sync(tempDir))
+  // Commit any diff screenshots to a remote, third-party repo.
+  .then(() => exec('git status screenshots-diff', { cwd: tempDir }))
+  .then((result) => {
+    const hasChanges = (result.indexOf('nothing to commit') > -1);
+
+    if (hasChanges) {
+      return Promise.reject(new Error('There are visual differences!'));
+    } else {
+      return Promise.resolve();
+    }
+  })
+
+  // Delete temp folder.
+  .then((result) => rimraf.sync(tempDir))
   .then(() => {
     console.log('DONE.');
     process.exit(0);
   })
   .catch((err) => {
-    console.log('ERROR.', err);
     throw err;
     process.exit(1);
   });
